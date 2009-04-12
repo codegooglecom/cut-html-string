a3d0871485e5cb37d435b0edb7e3cf4c8a2a5366
m.prajwala <m.prajwala@ddf513fe-2740-11de-9b42-3f9f9954309b>
2009-04-12 09:41:35 +0000

    Initial commit which is having javascript and php implementation of cut string

---
+++ javascript/test_cutstrin_ie.js
@@ -0,0 +1,84 @@
+/*jsl:option explicit*/
+/*jsl:declare inherit*/
+/*jsl:declare WebTestCase*/
+/*jsl:declare nsTEST*/
+/*jsl:declare CutString*/
+
+//create namespace
+if(typeof nsTEST == 'undefined'){
+    nsTEST = {};
+}
+
+nsTEST.include = ['./cutstring_ie.js'];
+
+nsTEST.cutString = inherit(WebTestCase);
+
+nsTEST.cutString.prototype.setUp = function(){
+};
+
+nsTEST.cutString.prototype.testCutString1 = function(){
+    var output = new CutString('word1,word2,word3',5);
+    this.assertEqual('word1',output.cut());
+};
+
+nsTEST.cutString.prototype.testCutString2 = function(){
+    var output = new CutString('<span id="1">Test</span>',5);
+    this.assertEqual('<span id="1">Test</span>',output.cut());
+};
+
+nsTEST.cutString.prototype.testCutString3 = function(){
+    var output = new CutString('<span id="1">Test Cutstring</span>',5);
+    this.assertEqual('<span id="1">Test </span>',output.cut());
+};
+
+nsTEST.cutString.prototype.testCutString4 = function(){
+    var output = new CutString('aa<span id="1">Test Cutstring</span>',5);
+    this.assertEqual('aa<span id="1">Tes</span>',output.cut());
+};
+
+nsTEST.cutString.prototype.testCutString5 = function(){
+    var output = new CutString('aa<span id="1">Test Cutstring</span> bb',5);
+    this.assertEqual('aa<span id="1">Tes</span>',output.cut());
+};
+
+nsTEST.cutString.prototype.testCutString6 = function(){
+    var output = new CutString('aa<span id="1">Test <b>Cutstring</b></span> bb',10);
+    this.assertEqual('aa<span id="1">Test <b>Cut</b></span>',output.cut());
+};
+
+nsTEST.cutString.prototype.testCutString7 = function(){
+    var output = new CutString('<span style="font-weight:bold">aa</span><span id="1">Test <b>Cutstring</b></span> bb',10);
+    var cutStr = output.cut();
+    var cmpStr = '<span style="font-weight: bold;">aa</span><span id="1">Test <b>Cut</b></span>';
+    this.assertEqual(cmpStr,cutStr);
+};
+
+nsTEST.cutString.prototype.testCutString8 = function(){
+    var output = new CutString('<span style="font-weight:bold">aa<em>BB</em></span><span id="1">Test <b>Cutstring</b></span> bb',10);
+    var cutStr = output.cut();
+    var cmpStr = '<span style="font-weight: bold;">aa<em>BB</em></span><span id="1">Test <b>C</b></span>';
+    this.assertEqual(cmpStr,cutStr);
+};
+
+nsTEST.cutString.prototype.testCutString9 = function(){
+    var output = new CutString('<p>aa<em>BB</em></p><br /><span id="1">Test <b>Cutstring</b></span> bb',10);
+    var cutStr = output.cut();
+    var cmpStr = '<p>aa<em>BB</em></p><br><span id="1">Test <b>C</b></span>';
+    this.assertEqual(cmpStr,cutStr);
+};
+
+nsTEST.cutString.prototype.testCutString10 = function(){
+    var output = new CutString('<p>aa<em>BB</em></p><ul><li>one</li><li>two</li><li>three</li></ul>',10);
+    var cutStr = output.cut();
+    var cmpStr = '<p>aa<em>BB</em></p><ul><li>one</li><li>two</li></ul>';
+    this.assertEqual(cmpStr,cutStr);
+};
+
+nsTEST.cutString.prototype.testCutString11 = function(){
+    var output = new CutString('<span>aa<em>BB</em><ul><li>one</li><li>two</li><li>three</li></ul></span>',10);
+    var cutStr = output.cut();
+    var cmpStr = '<span>aa<em>BB</em><ul><li>one</li><li>two</li></ul></span>';
+    this.assertEqual(cmpStr,cutStr);
+};
+
+
---
+++ php/tests/test_cutstring.php
@@ -0,0 +1,80 @@
+<?php
+require_once(dirname(__FILE__) . '/simpletest/autorun.php');
+require_once('../cutstring.php');
+
+
+class TestCutstring extends UnitTestCase {
+    function testdata1() {
+      $data = 'aaa<span id="1">Test Cutstring</span>';
+      $output = new HtmlCutString($data,5);
+      
+      $this->assertEqual('aaa<span id="1">Te</span>'."\n",$output->cut());
+      //echo $output->cut()."\n";
+      //echo 'aaa<span id="1">Te</span>';
+    }
+    function testdata2(){
+      $data = 'word1,word2,word3';
+      $output = new HtmlCutString($data,5);
+      
+      $this->assertEqual('word1'."\n",$output->cut());
+    }
+
+    function testdata3(){
+      $data = '<span id="1">Test</span>';
+      $output = new HtmlCutString($data,5);
+      
+      $this->assertEqual('<span id="1">Test</span>'."\n",$output->cut());
+    }
+    function testdata4(){
+      $data = '<span id="1">Test Cutstring</span>';
+      $output = new HtmlCutString($data,5);
+      
+      $this->assertEqual('<span id="1">Test </span>'."\n",$output->cut());
+    }
+    function testdata5(){
+      $data = 'aa<span id="1">Test Cutstring</span> bb';
+      $output = new HtmlCutString($data,5);
+      
+      $this->assertEqual('aa<span id="1">Tes</span>'."\n",$output->cut());
+    }
+    function testdata6(){
+      $data = 'aa<span id="1">Test <b>Cutstring</b></span> bb';
+      $output = new HtmlCutString($data,10);
+      
+      $this->assertEqual('aa<span id="1">Test <b>Cut</b></span>'."\n",$output->cut());
+    }
+    function testdata7(){
+      $data = '<span style="font-weight:bold">aa</span><span id="1">Test <b>Cutstring</b></span> bb';
+      $output = new HtmlCutString($data,10);
+      
+      $this->assertEqual('<span style="font-weight:bold">aa</span><span id="1">Test <b>Cut</b></span>'."\n",$output->cut());
+    }
+
+    function testdata8(){
+      $data = '<span style="font-weight:bold">aa<em>BB</em></span><span id="1">Test <b>Cutstring</b></span> bb';
+      $output = new HtmlCutString($data,10);
+      
+      $this->assertEqual('<span style="font-weight:bold">aa<em>BB</em></span><span id="1">Test <b>C</b></span>'."\n",$output->cut());
+    }
+    function testdata9(){
+      $data = '<p>aa<em>BB</em></p><br /><span id="1">Test <b>Cutstring</b></span> bb';
+      $output = new HtmlCutString($data,10);
+      
+      $this->assertEqual('<p>aa<em>BB</em></p><br><span id="1">Test <b>C</b></span>'."\n",$output->cut());
+    }
+    function testdata10(){
+      $data = '<p>aa<em>BB</em></p><ul><li>one</li><li>two</li><li>three</li></ul>';
+      $output = new HtmlCutString($data,10);
+      // adding \n for the blocked elements. but this is not a big problem
+      $this->assertEqual("<p>aa<em>BB</em></p><ul>\n<li>one</li>\n<li>two</li>\n</ul>"."\n",$output->cut());
+    }
+
+    function testdata11(){
+      $data = '<span>aa<em>BB</em><ul><li>one</li><li>two</li><li>three</li></ul></span>';
+      $output = new HtmlCutString($data,10);
+      // adding \n for the blocked elements. but this is not a big problem
+      $this->assertEqual("<span>aa<em>BB</em><ul>\n<li>one</li>\n<li>two</li>\n</ul></span>"."\n",$output->cut());
+    }
+
+
+}
